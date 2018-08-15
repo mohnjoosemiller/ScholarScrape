eacf59a7e66642ad7c7c9883328818d7b74da999
John Moosemiller <itcaughtonj@gmail.com>
2018-08-14 20:31:47 -0500

    Add search option functionality

--- ScholarScrape.m
+++ ScholarScrape.m
@@ -1,12 +1,16 @@
 clear all
-st = input('What is your query?')
-
+st = inputdlg({'What is your query?'});
+pgs = inputdlg({'how many pages of results to query?'});
+inc_cit = listdlg("ListString", {'yes','no'},"PromptString",'Do you want to include citations?')-1;
+inc_pat = listdlg("ListString", {'yes','no'},"PromptString",'Do you want to include patents?')-1;
+date_beg = inputdlg({'Start Date of Query?'});
+date_end = inputdlg({'End Date of Query?'});
 
 c=1
-pgs = input('how many pages of results to query?')
-for p = 1:pgs
+
+for p = 1:pgs{1}
   p=p-1
-  url = ['https://scholar.google.com/scholar?hl=en&as_sdt=0%2C44&q=filetype%3Apdf+',st,'&start=',num2str(p*10)];
+  url = ['https://scholar.google.com/scholar?hl=en&as_ylo=',num2str(date_beg{1}),'&as_yhi=',num2str(date_end{1}),'&as_sdt=',num2str(inc_pat),'&as_vis=',num2str(inc_cit),'%2C44&q=filetype%3Apdf+',st{1},'&start=',num2str(p*10)];
   a = urlread(url);
   URLS = unique(regexp(a,'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])','match'));
   for i = 1:length(URLS)
